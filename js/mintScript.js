a6aa6911e50bf5fe2e53d14874fdf93eec67b0e8
james <james.roy.1252@gmail.com>
2022-04-14 20:37:07 +0900

    test

--- js/mintScript.js
+++ js/mintScript.js
@@ -823,107 +823,7 @@ async function connect() {
   });
   await check_status();
 }
-async function check_status() {
-  const PUBLIC = 2;
-  const WHITELIST = 1;
-  const SPECIAL = 0;
-  await myContract.methods
-    .balanceOf(account)
-    .call()
-    .then((result2) => {
-      balanceOfAccount = parseInt(result2);
-    });
-
-  await myContract.methods
-    .mintingInformation()
-    .call()
-    .then(async function (result) {
-      mintIndexForSale = parseInt(result[1]);
-      mintLimitPerBlock = parseInt(result[2]);
-      mintLimitPerSale = parseInt(result[3]);
-      mintStartBlockNumber = parseInt(result[4]);
-      maxSaleAmount = parseInt(result[5]);
-      mintPrice = parseInt(result[6]);
-      round = parseInt(result[7]);
-      if (round == SPECIAL) {
-        document.querySelector("progress").max = maxSaleAmount;
-        document.querySelector("progress").value = mintIndexForSale;
-        document.getElementById("round").innerHTML = "<p>Round</p>\n" + "<p>Special</p>";
-        document.getElementById("count").innerHTML = `남은 수량 :${maxSaleAmount - (mintIndexForSale - 1)}`;
-        await myContract.methods
-          .isSpecialList(account)
-          .call()
-          .then((result2) => {
-            document.getElementById("holdnfts").style.display = "none";
-            document.getElementById("approval").style.display = "block";
-            document.querySelector("#approval p:first-child").innerHTML = "Speical";
-            if (result2 == true) {
-              document.querySelector("#approval p:last-child").style.color = "#1cb80f";
-              document.querySelector("#approval p:last-child").innerHTML = "✅ Approved";
-            } else {
-              document.querySelector("#approval p:last-child").style.color = "#94160d";
-              document.querySelector("#approval p:last-child").innerHTML = "Not Approved";
-            }
-          });
-      } else if (round == WHITELIST) {
-        document.querySelector("progress").max = maxSaleAmount - 20;
-        document.querySelector("progress").value = mintIndexForSale - 20;
-        document.getElementById("round").innerHTML = "<p>Round</p>\n" + "<p>Whitelist</p>";
-        document.getElementById("count").innerHTML = `남은 수량 :${maxSaleAmount - (mintIndexForSale - 1)}`;
-        await myContract.methods
-          .isWhiteList(account)
-          .call()
-          .then((result2) => {
-            document.getElementById("holdnfts").style.display = "none";
-            document.getElementById("approval").style.display = "block";
-            document.querySelector("#approval p:first-child").innerHTML = "WhiteList";
-            if (result2 == true) {
-              document.querySelector("#approval p:last-child").style.color = "#1cb80f";
-              document.querySelector("#approval p:last-child").innerHTML = "✅ Approved";
-            } else {
-              document.querySelector("#approval p:last-child").style.color = "#94160d";
-              document.querySelector("#approval p:last-child").innerHTML = "Not Approved";
-            }
-          });
-      } else if (round == PUBLIC) {
-        document.querySelector("progress").max = maxSaleAmount;
-        document.querySelector("progress").value = mintIndexForSale;
-        document.getElementById("round").innerHTML = "<p>Round</p>\n" + "<p>Public</p>";
-        document.getElementById("count").innerHTML = `남은 수량 :${maxSaleAmount - (mintIndexForSale - 1)}`;
-        document.getElementById("approval").style.display = "none";
-        document.getElementById("holdnfts").style.display = "block";
-        document.querySelector("#holdnfts p:last-child").innerHTML = String(balanceOfAccount);
-      } else {
-        document.querySelector("progress").max = maxSaleAmount;
-        document.getElementById("round").innerHTML = "<p>Round</p>\n" + "<p>None</p>";
-        document.getElementById("count").innerHTML = `남은 수량 :${maxSaleAmount - (mintIndexForSale - 1)}`;
-        document.getElementById("approval").style.display = "none";
-        document.getElementById("holdnfts").style.display = "block";
-        document.querySelector("#holdnfts p:last-child").innerHTML = String(balanceOfAccount);
-      }
-      if (document.querySelector("progress").value / (document.querySelector("progress").max / 85) > 3) {
-        document.getElementById("amount_sign").style.left =
-          document.querySelector("progress").value / (document.querySelector("progress").max / 85) + "%";
-      } else {
-        document.getElementById("amount_sign").style.left = "3%";
-      }
-      document.getElementById("pertransacion").innerHTML = "<p>Per Transacion</p>\n" + `<p>${mintLimitPerBlock}</p>`;
-      document.getElementById("mintingstartsat").innerHTML =
-        "<p>MINTING STARTS AT</p>\n" + `<p>#${mintStartBlockNumber}</p>`;
-      document.getElementById("price").innerHTML =
-        "<p>Price</p>\n" + `<p>${caver.utils.fromPeb(mintPrice, "KLAY")}</p>`;
-      document.getElementById("perwallet").innerHTML = "<p>Per Wallet</p>\n" + `<p>${mintLimitPerSale}</p>`;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  if (!blockCnt) {
-    blockNumber = await caver.klay.getBlockNumber();
-    /*document.getElementById("currentblock").innerHTML =
-    "<p>CURRENT BLOCK</p>\n" + `<p>#${blockNumber}</p>`;*/
-    cntBlockNumber();
-  }
-}
+async function check_status() {}
 
 async function allMint() {
   await connect();
@@ -986,12 +886,13 @@ async function publicMint() {
   const total_value = amount * mintPrice;
   let estmated_gas;
   await myContract.methods
-    .publicMint(amount)
+    .approve(account, amount)
     .estimateGas({ from: account, gas: 6000000, value: total_value })
     .then(function (gasAmount) {
       estmated_gas = gasAmount;
       myContract.methods
-        .publicMint(amount)
+        // .approve(amount)
+        .transfer(account, amount)
         .send({ from: account, gas: estmated_gas, value: total_value })
         .on("transactionHash", (txid) => {})
         .once("allEvents", (allEvents) => {})
